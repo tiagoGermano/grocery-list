a3edab2531434b1affa44eb006e1af6702dac4de
Tiago Germano <tiagogermanos@gmail.com>
2023-11-23 11:21:48 -0300

    initial set of project, configured h2 database (temporary) and mapped first entity

---
+++ src/main/java/com/fortalsoft/grocery/entity/Product.java
@@ -0,0 +1,34 @@
+package com.fortalsoft.grocery.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String description;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
